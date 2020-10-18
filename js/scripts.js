69b6d7f44f2ab86a2a5e3dc953225d7ed5558daa
MAN95-dev <michelleamynewell@gmail.com>
2020-10-18 15:42:17 +0000

    aaded function for game timer

--- js/scripts.js
+++ js/scripts.js
@@ -161,7 +161,24 @@ function moveCounter(){
     }
 }
 
-
+// @description game timer
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second=0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
 
 
 
