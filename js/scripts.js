b7135f13a5b4b799b6a214d049948087b94ae0f7
MAN95-dev <michelleamynewell@gmail.com>
2020-10-18 09:57:07 +0000

    declared star icons

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ let cards = [...card];
 
 // deck of all cards in game
 const deck = document.getElementById("card-deck");
+
+// declaring move variable
+let moves = 0;
+let counter = document.querySelector(".moves");
+
+// declare variables for star icons
+const stars = document.querySelectorAll(".fa-star");
